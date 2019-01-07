840bb3dccf82c3c5a48fb9331d705126dc227c74
returnes@163.com <returnes@163.com>
2019-01-07 20:21:30 +0800

    注册功能

--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -6,6 +6,7 @@ from django.views import View
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 
 from users.models import User
 from django_redis import get_redis_connection
@@ -44,16 +45,20 @@ class RegisterMobileCountView(APIView):
             'mobile':mobile
         }
         return Response(context)
+#
+# class RegisterView(APIView):
+#     '''
+#     注册功能
+#     post:http://127.0.0.1:8000/users/
+#     '''
+#     def post(self,request):
+#         data=request.data #获取前端提交数据
+#         serializer=RegisterModelSerializers(data=data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#
+#         return Response(serializer.data)
 
-class RegisterView(APIView):
-    '''
-    注册功能
-    post:http://127.0.0.1:8000/users/
-    '''
-    def post(self,request):
-        data=request.data #获取前端提交数据
-        serializer=RegisterModelSerializers(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
+class RegisterView(CreateAPIView):
+    # queryset = User.objects.all()
+    serializer_class = RegisterModelSerializers
